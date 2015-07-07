79b7c4aef9647d5ce2eae0d9fe343dffcd62502f
Andy Mishra <andy.mishra@opower.com>
2015-07-07 16:59:41 -0400

    refactor

--- js/app.js
+++ js/app.js
@@ -3,20 +3,23 @@ $(document).ready(function() {
 		for (i=1; i<=n; i++) {
 			if (i % 3 == 0 && i % 5 == 0) {
 				$("body").append("fizz buzz<br>");
-				continue;
 			}
 			else if (i % 3 == 0) {
 				$("body").append("fizz<br>");
-				continue;
 			}
 			else if (i % 5 == 0) {
 				$("body").append("buzz<br>");
-				continue;
 			}
 			else {
-				$("body").append("" + i + "<br>");
+				$("body").append(i + "<br>");
 			}
 		}
 	}
-	fizzBuzz(100);
-});
+
+	var input = prompt('Please supply a number');
+	while (input == "" || isNaN(parseInt(input))) {
+		var input = prompt("You didn't enter a valid number. Try again.");
+	}
+
+	fizzBuzz(+parseInt(input));
+})
